ca5c7a5efa812afd0993314574d8f6a546ed217a
Richard Kellner <richard.kellner@gmail.com>
2017-03-25 13:29:08 +0100

    Updated copyright year

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Ludolph: Weather plugin
-# Copyright (C) 2015 Erigones, s. r. o.
+# Copyright (C) 2015-2017 Erigones, s. r. o.
 #
 # See the LICENSE file for copying permission.
 
